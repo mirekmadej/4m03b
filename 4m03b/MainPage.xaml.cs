755139f200681ffd5a2f420a87fb7a49f98bc664
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-09-22 13:42:15 +0200

    Dodaj pliki projektów.

---
+++ 4m03b/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace _4m03b
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string? ent = emailEnt.Text;
+            bool czyOk = false;
+            if (ent != null)
+            {
+               if( ent.Contains('@'))
+                {       
+                    if(ent.IndexOf('@')>0 && ent.IndexOf('@')<ent.Length-1)
+                        czyOk = true;
+                }
+            }
+
+            if(czyOk)
+            {
+                weryfikacjaLbl.Text = "poprawny";
+            }
+            else
+            {
+                weryfikacjaLbl.Text = "zły";
+            }
+            SemanticScreenReader.Announce(weryfikacjaLbl.Text);
+        }
+    }
+}
